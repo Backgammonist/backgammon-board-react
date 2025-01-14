d26a76c3cfa1c9ac46fb02617ba47831319042d1
Laszlo Heves <heves.laszlo@gmail.com>
2025-01-14 11:14:24 +0000

    fix: add maxDiffPixelRatio to pw tests

--- test-harness/tests/tests.spec.ts
+++ test-harness/tests/tests.spec.ts
@@ -2,15 +2,15 @@ import { test, expect } from "@playwright/test";
 
 test("overload", async ({ page }) => {
   await page.goto("/tests/index.html?position=overload");
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.01 });
 });
 
 test("default", async ({ page }) => {
   await page.goto("/tests/index.html?position=default");
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.01 });
 });
 
 test("crazy", async ({ page }) => {
   await page.goto("/tests/index.html?position=default&theme=crazy");
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.01 });
 });
