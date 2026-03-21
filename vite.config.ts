da238194367bbfe08597bf1804cb16184e68fa1a
Laszlo Heves <heves.laszlo@gmail.com>
2026-03-21 19:54:44 +0000

    fix: fixups

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), dts({ include: ["src"] })],
-  test: {
-    environment: "jsdom",
-    include: ["src/**/*.{test,spec}.{ts,tsx}"],
-    setupFiles: ["./src/test/setup.ts"],
-    passWithNoTests: true,
-  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.tsx"),
       name: "BackgammonBoard",
       fileName: (format) => `index.${format === "es" ? "js" : "umd.cjs"}`,
     },
-    // sourcemap: true,
     rollupOptions: {
       external: ["react", "react-dom"],
       output: {
