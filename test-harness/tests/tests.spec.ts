32d7a4bdb0262a8575160616d3ab3ca6e518842e
Laszlo Heves <heves.laszlo@gmail.com>
2026-03-22 22:31:08 +0000

    test: update e2es

--- test-harness/tests/tests.spec.ts
+++ test-harness/tests/tests.spec.ts
@@ -1,16 +1,38 @@
 import { test, expect } from "@playwright/test";
 
-test("overload", async ({ page }) => {
-  await page.goto("/tests/index.html?position=overload");
-  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.01 });
-});
+[
+  { direction: "clockwise", dice: "3,5", doublingCube: { value: 16 } },
+  {
+    direction: "anticlockwise",
+    dice: "2,6",
+    doublingCube: { value: 32, owner: "player" },
+  },
+  { direction: "anticlockwise", dice: "1,3" },
+].forEach(({ direction, dice, doublingCube }) => {
+  test(`overload - direction: ${direction} - dice: ${dice} - doublingValue: ${doublingCube?.value ?? "N/A"} - doublingOwner: ${doublingCube?.owner ?? "N/A"}`, async ({
+    page,
+  }) => {
+    await page.goto(
+      `/tests/index.html?position=overload&direction=${direction}&dice=${dice}&doublingValue=${doublingCube?.value}&doublingOwner=${doublingCube?.owner}`,
+    );
+    await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.01 });
+  });
 
-test("default", async ({ page }) => {
-  await page.goto("/tests/index.html?position=default");
-  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.01 });
-});
+  test(`default - direction: ${direction} - dice: ${dice} - doublingValue: ${doublingCube?.value ?? "N/A"} - doublingOwner: ${doublingCube?.owner ?? "N/A"}`, async ({
+    page,
+  }) => {
+    await page.goto(
+      `/tests/index.html?position=default&direction=${direction}&dice=${dice}&doublingValue=${doublingCube?.value}&doublingOwner=${doublingCube?.owner}`,
+    );
+    await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.01 });
+  });
 
-test("crazy", async ({ page }) => {
-  await page.goto("/tests/index.html?position=default&theme=crazy");
-  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.01 });
+  test(`crazy - direction: ${direction} - dice: ${dice} - doublingValue: ${doublingCube?.value ?? "N/A"} - doublingOwner: ${doublingCube?.owner ?? "N/A"}`, async ({
+    page,
+  }) => {
+    await page.goto(
+      `/tests/index.html?position=default&theme=crazy&direction=${direction}&dice=${dice}&doublingValue=${doublingCube?.value}&doublingOwner=${doublingCube?.owner}`,
+    );
+    await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.01 });
+  });
 });
